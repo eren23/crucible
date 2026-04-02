15ee6ffad79ffae88013595909a9618626cc3b41
eren23 <erenak1996@gmail.com>
2026-04-02 17:23:43 +0300

    feat(data_sources): export all three built-in plugins

--- src/crucible/data_sources/__init__.py
+++ src/crucible/data_sources/__init__.py
@@ -2,7 +2,11 @@
 from __future__ import annotations
 
 from crucible.data_sources.huggingface import HuggingFaceDataSource
+from crucible.data_sources.wandb_artifact import WandBArtifactSource
+from crucible.data_sources.local_files import LocalFilesSource
 
 __all__ = [
     "HuggingFaceDataSource",
+    "WandBArtifactSource",
+    "LocalFilesSource",
 ]
